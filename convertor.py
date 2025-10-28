40d164a3034c0d3912cc07acce4231280d3cc3da
Leonid <bohdanovleonid93@gmail.com>
2025-10-28 10:29:26 +0200

    add main logic in def main()

--- convertor.py
+++ convertor.py
@@ -37,3 +37,38 @@ def main():
         print("7: Exit")
 
         choice = input("Make your choice: ")
+        if choice == "1":
+            km_str = input("Enter the number of kilometers: ")
+            km = float(km_str)
+            miles = kilometers_to_miles(km)
+            print(f"Result: {km} km = {miles:.2f} miles")
+        elif choice == "2":
+            miles_str = input("Enter the number of miles: ")
+            miles = float(miles_str)
+            km = miles_to_kilometers(miles)
+            print(f"Result: {miles} miles = {km:.2f} km")
+        elif choice == "3":
+            kg_str = input("Enter the number of kilograms: ")
+            kg = float(kg_str)
+            pounds = kg_to_pounds(kg)
+            print(f"Result: {kg} kg = {pounds:.2f} pounds")
+        elif choice == "4":
+            pounds_str = input("Enter the number of pounds: ")
+            pounds = float(pounds_str)
+            kg = pounds_to_kg(pounds)
+            print(f"Result: {pounds} pounds = {kg:.2f} kg")
+        elif choice == "5":
+            celsius_str = input("Enter the number of celsius: ")
+            celsius = float(celsius_str)
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {celsius} celsius = {fahrenheit:.2f} fahrenheit")
+        elif choice == "6":
+            fahrenheit_str = input("Enter the number of fahrenheit: ")
+            fahrenheit = float(fahrenheit_str)
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {fahrenheit} fahrenheit = {celsius:.2f} celsius")
+        elif choice == "7":
+            print("Thank you for using this convertor. Goodbye")
+            break
+        else:
+            print("Invalid choice. Try again")
