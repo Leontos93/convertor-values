c502b790f12a483675bcfa3a96047b1a9ae6d220
Leonid <bohdanovleonid93@gmail.com>
2025-10-28 10:04:45 +0200

    add round to the all def's

--- convertor.py
+++ convertor.py
@@ -1,17 +1,17 @@
 def kilometers_to_miles(kilometers):
-    return kilometers * 0.6213171
+    return round(kilometers * 0.6213171, 2)
 
 
 def miles_to_kilometers(miles):
-    return miles * 1.60934
+    return round(miles * 1.60934, 2)
 
 
 def kg_to_pounds(kg):
-    return kg * 2.20462
+    return round(kg * 2.20462, 2)
 
 
 def pounds_to_kg(pounds):
-    return pounds / 2.20462
+    return round(pounds / 2.20462, 2)
 
 
 def celsium_to_fahrenheit(celsius):
