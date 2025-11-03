58528dd605cbd92713657fccfb7a284e3aab4f94
Leonid <bohdanovleonid93@gmail.com>
2025-11-03 07:59:47 +0200

    add def main()

--- modify_convertor.py
+++ modify_convertor.py
@@ -20,3 +20,14 @@ def celsius_to_fahrenheit(val):
 
 def fahrenheit_to_celsius(val):
     return (val - 32) * 5 / 9
+
+
+def main():
+    conversions = {
+        "1": ("Kilometers -> Miles", kilometers_to_miles, "km", "miles"),
+        "2": ("Miles -> Kilometers", miles_to_kilometers, "miles", "km"),
+        "3": ("Kilograms -> Pounds", kg_to_pounds, "kg", "pounds"),
+        "4": ("Pounds -> Kilograms", pounds_to_kg, "pounds", "kg"),
+        "5": ("Celsius -> Fahrenheit", celsius_to_fahrenheit, "celsius", "fahrenheit"),
+        "6": ("Fahrenheit -> Celsius", fahrenheit_to_celsius, "fahrenheit", "celsius"),
+    }
