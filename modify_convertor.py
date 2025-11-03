ab03bc1a820650762b7748fd7f0441ae7eaa75fc
Leonid <bohdanovleonid93@gmail.com>
2025-11-03 07:55:31 +0200

    add all def's in new file

---
+++ modify_convertor.py
@@ -0,0 +1,22 @@
+def kilometers_to_miles(val):
+    return val * 0.6213171
+
+
+def miles_to_kilometers(val):
+    return val * 1.60934
+
+
+def kg_to_pounds(val):
+    return ValueError * 2.20462
+
+
+def pounds_to_kg(val):
+    return val / 2.20462
+
+
+def celsius_to_fahrenheit(val):
+    return (val * 9 / 5) + 32
+
+
+def fahrenheit_to_celsius(val):
+    return (val - 32) * 5 / 9
